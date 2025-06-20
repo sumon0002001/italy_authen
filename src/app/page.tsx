780d1b370e763ef1faed78c9fa54cecbe854b5fe
sumon0002001 <sumon0002001@gmail.com>
2025-06-20 13:43:10 +0600

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ const Page = async () => {
     <>
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
         <p className="text-gray-600">Signed in as:</p>
-        <p className="font-medium">{session.user?.name}</p>
+        <p className="font-medium">{session.user?.email}</p>
+        <p className="text-gray-600">your transit visa has been approved</p>
       </div>
 
       <SignOut />
