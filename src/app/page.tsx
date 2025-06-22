a2fd8051ca6da3db36238bdfb2b4ae5d261ef375
sumon0002001 <sumon0002001@gmail.com>
2025-06-22 22:29:54 +0600

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Page = async () => {
       <div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
         <h1 className="text-blue-700 font-serif font-semibold text-xl">Visa Verification Enquiry</h1>
         <p className="text-gray-600">Enter the details of the visa to be verified </p>
-        <p className="font-medium">Please enter these details exactly as they appear in the visa holder's current passport</p>
+        <p className="font-medium">Please enter these details exactly as they appear in the visa holder current passport</p>
         
         <p className="text-gray-600">your transit visa has been approved</p>
       </div>
